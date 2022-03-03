caa3e7db0dd3233f81e9cf479b06b6f2052fcc47
Alex Aritan <cokethenwpepsi@gmail.com>
2022-03-03 00:16:30 -0500

    format the images in a grid format for desktops and a list for phones

--- src/index.tsx
+++ src/index.tsx
@@ -11,8 +11,14 @@ ReactDOM.render(
 				'https://i.imgur.com/MSCTOFy.jpg',
 				'https://i.imgur.com/WJbOiGQ.png',
 				'https://i.imgur.com/6kUOwrF.jpg',
+				'https://i.imgur.com/MSCTOFy.jpg',
+				'https://i.imgur.com/7ZbuWh1.jpg',
+				'https://i.imgur.com/MSCTOFy.jpg',
+				'https://i.imgur.com/WJbOiGQ.png',
+				'https://i.imgur.com/6kUOwrF.jpg',
 				'https://i.imgur.com/7ZbuWh1.jpg',
 			]}
+			title="Photos"
 		/>
 	</React.StrictMode>,
 	document.getElementById('root')
--- src/photo-grid/PhotoGrid.tsx
+++ src/photo-grid/PhotoGrid.tsx
@@ -1,11 +1,37 @@
+import { ImageListItem } from '@mui/material';
 import ImageList from '@mui/material/ImageList';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
-export const PhotoGrid = ({ imageUrls }: { imageUrls: string[] }) => {
+export const PhotoGrid = ({
+	imageUrls,
+	title,
+}: {
+	imageUrls: string[];
+	title: string;
+}) => {
+	const theme = useTheme();
+	const shouldRenderMultipleColumns = useMediaQuery(theme.breakpoints.up('sm'));
 	return (
-		<ImageList cols={3} rowHeight={500} sx={{ height: 500, width: 500 }}>
-			{imageUrls.map(url => (
-				<img src={url} />
-			))}
-		</ImageList>
+		<div
+			className="photoGrid"
+			style={{ width: shouldRenderMultipleColumns ? 1000 : '100%' }}
+		>
+			<h1>{title}</h1>
+			<ImageList
+				cols={shouldRenderMultipleColumns ? 3 : 1}
+				rowHeight={shouldRenderMultipleColumns ? 200 : 300}
+			>
+				{imageUrls.map((url, index) => (
+					<ImageListItem
+						className="photo"
+						key={`photo${index}`}
+						style={{ overflow: 'hidden' }}
+					>
+						<img src={url} />
+					</ImageListItem>
+				))}
+			</ImageList>
+		</div>
 	);
 };
