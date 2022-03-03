ad96b50b8d3379b2f8577c358f6c941965a6feee
Alex Aritan <cokethenwpepsi@gmail.com>
2022-03-03 02:28:36 -0500

    added dark mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { NavigationBar } from './navigation/NavigationBar';
 import { NavigationDrawer } from './navigation/NavigationDrawer';
 import { Home } from './Home';
 import { Example } from './albums/Example';
+import { useMediaQuery } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 export const App = () => {
 	const [navIsOpen, setNavOpen] = useState(false);
+	//TODO Alex, this line below returns false then true immediately after.
+	//It would be nice if it could just return the proper value immediately.
+	//That way dark mode would be remembered.
+	const defaultToDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+	const [isInDarkMode, setDarkMode] = useState(defaultToDarkMode);
+
+	const theme = useMemo(
+		() =>
+			createTheme({
+				palette: {
+					mode: isInDarkMode ? 'dark' : 'light',
+				},
+			}),
+		[isInDarkMode]
+	);
 
 	return (
-		<>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
 			<NavigationBar
+				isInDarkMode={isInDarkMode}
 				navIsOpen={navIsOpen}
-				setNavOpen={() => setNavOpen(!navIsOpen)}
+				setDarkMode={setDarkMode}
+				toggleNavigationDrawer={() => setNavOpen(!navIsOpen)}
 			/>
 			<NavigationDrawer navIsOpen={navIsOpen} setNavOpen={setNavOpen} />
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='example' element={<Example />} />
 			</Routes>
-		</>
+		</ThemeProvider>
 	);
 };
--- src/navigation/NavigationBar.tsx
+++ src/navigation/NavigationBar.tsx
@@ -1,13 +1,19 @@
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import ModeNightIcon from '@mui/icons-material/ModeNight';
 import { Toolbar } from '@mui/material';
 
 export const NavigationBar = ({
+	isInDarkMode,
 	navIsOpen,
-	setNavOpen,
+	setDarkMode,
+	toggleNavigationDrawer,
 }: {
+	isInDarkMode: boolean;
 	navIsOpen: boolean;
-	setNavOpen: () => void;
+	setDarkMode: (setToDark: boolean) => void;
+	toggleNavigationDrawer: () => void;
 }) => {
 	return (
 		<Toolbar>
@@ -15,9 +21,17 @@ export const NavigationBar = ({
 				aria-label='open drawer'
 				color='inherit'
 				edge='start'
-				onClick={setNavOpen}
+				onClick={toggleNavigationDrawer}
 			>
 				<MenuIcon fontSize='large' />
+			</IconButton>
+			<IconButton
+				aria-label='dark mode toggle'
+				color='inherit'
+				edge='end'
+				onClick={() => setDarkMode(!isInDarkMode)}
+			>
+				{isInDarkMode ? <Brightness7Icon /> : <ModeNightIcon />}
 			</IconButton>
 		</Toolbar>
 	);
