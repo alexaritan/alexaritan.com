de32400216a7a8308f2bc20094bff96f49b20d30
Alex Aritan <cokethenwpepsi@gmail.com>
2022-03-02 23:14:56 -0500

    ejected react app

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { PhotoGrid } from './photo-grid/PhotoGrid.tsx';
+import { PhotoGrid } from './photo-grid/PhotoGrid';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
