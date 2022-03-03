9c3857c734e637465c79abfacc754ff025885761
Alex Aritan <cokethenwpepsi@gmail.com>
2022-03-03 11:16:58 -0500

    hide close photo icon on mobile

--- src/photo-grid/FullPagePhoto.tsx
+++ src/photo-grid/FullPagePhoto.tsx
@@ -1,15 +1,18 @@
-import { Dialog } from '@mui/material';
+import { Dialog, useMediaQuery } from '@mui/material';
 import { useCallback } from 'react';
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
-import { albumUrl, parseAlbumFromUrl } from '../utils/urls';
 import CloseIcon from '@mui/icons-material/Close';
 import { IconButton } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { albumUrl, parseAlbumFromUrl } from '../utils/urls';
 
 export const FullPagePhoto = () => {
 	const [searchParams] = useSearchParams();
 	const src = searchParams.get('src');
 	const { pathname } = useLocation();
 	const navigate = useNavigate();
+	const theme = useTheme();
+	const shouldRenderNavIcons = useMediaQuery(theme.breakpoints.up('sm'));
 
 	const handleClose = useCallback(
 		() =>
@@ -19,13 +22,15 @@ export const FullPagePhoto = () => {
 
 	return (
 		<Dialog fullScreen open={true}>
-			<IconButton
-				edge='start'
-				onClick={handleClose}
-				style={{ left: 20, position: 'absolute', top: 10 }}
-			>
-				<CloseIcon />
-			</IconButton>
+			{shouldRenderNavIcons ? (
+				<IconButton
+					edge='start'
+					onClick={handleClose}
+					style={{ left: 20, position: 'absolute', top: 10 }}
+				>
+					<CloseIcon />
+				</IconButton>
+			) : null}
 			{src ? (
 				<img
 					src={src}
