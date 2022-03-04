70c5862bbc743157ecc207156db0f0d95477d98a
Alex Aritan <cokethenwpepsi@gmail.com>
2022-03-03 19:46:44 -0500

    added sliding, fading animation to the home page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,13 @@
 import { Box } from '@mui/material';
+import { FadeAndSlide } from './animation/FadeAndSlide';
 
 export const Home = () => (
-	<Box textAlign='center'>
+	<Box style={{ marginTop: 100 }} textAlign='center'>
 		<div>
-			<img src='https://i.imgur.com/UZ3FIxO.jpg' width={'100%'} />
+			{/* <img src='https://i.imgur.com/UZ3FIxO.jpg' width={'100%'} /> */}
+			<FadeAndSlide
+				words={['Queenstown', 'Budapest', 'Lauterbrunnen', 'Bavaria']}
+			/>
 		</div>
 	</Box>
 );
